8c8529fd47ce0776fcbc6e8cf8c56e1cfaf95f37
Cameron Romo <cromo1225@gmail.com>
2021-04-16 14:23:56 -0600

    Add axios post method to add a post

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from 'axios';
 
+const apiEndpoint = 'http://jsonplaceholder.typicode.com/posts';
+
 class App extends Component {
   state = {
     posts: []
@@ -9,12 +11,16 @@ class App extends Component {
 
   async componentDidMount() {
     // pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await axios.get('http://jsonplaceholder.typicode.com/posts');
+    const { data: posts } = await axios.get(apiEndpoint);
     this.setState({ posts });
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = { title: 'a', body: 'b' }
+    const { data: post } = await axios.post(apiEndpoint, obj);
+    
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
   handleUpdate = post => {
