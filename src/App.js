deff486b50bac800167a95919c2913dc919ebdff
Cameron Romo <cromo1225@gmail.com>
2021-04-17 13:21:42 -0600

    Extract a config module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import http from './services/httpService';
 import "./App.css";
-
-const apiEndpoint = 'http://jsonplaceholder.typicode.com/posts';
-
+import config from './config.json'
 class App extends Component {
   state = {
     posts: []
@@ -11,13 +9,13 @@ class App extends Component {
 
   async componentDidMount() {
     // pending > resolved (success) OR rejected (failure)
-    const { data: posts } = await http.get(apiEndpoint);
+    const { data: posts } = await http.get(config.apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: 'a', body: 'b' }
-    const { data: post } = await http.post(apiEndpoint, obj);
+    const { data: post } = await http.post(config.apiEndpoint, obj);
     
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -25,7 +23,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "UPDATED";
-    await http.put(apiEndpoint + '/' + post.id, post);
+    await http.put(config.apiEndpoint + '/' + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -41,7 +39,7 @@ class App extends Component {
     
     // Use try catch for specific errors, otherwise use the interceptor
     try{
-      await http.delete(apiEndpoint + '/' + post.id);
+      await http.delete(config.apiEndpoint + '/' + post.id);
     } catch (ex) {
       // Expected (404: not found, 400: bad request) - Client errors
       // - Display a specific error message 
